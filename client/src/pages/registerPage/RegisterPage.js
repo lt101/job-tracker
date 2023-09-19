0db398d3af53850972de1d2c719b41150cce6de9
Liem <liem.truong@polymtl.ca>
2023-09-19 19:22:38 -0400

    create register page

--- client/src/pages/registerPage/RegisterPage.js
+++ client/src/pages/registerPage/RegisterPage.js
@@ -1,7 +1,125 @@
-import React from "react";
+import React, { useState } from "react";
+import MainScreen from "../../components/mainScreen/MainScreen";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import Loading from "../../components/Loading";
+import ErrorMessage from "../../components/ErrorMessage";
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  const defaultUser = {
+    name: "",
+    email: "",
+    password: "",
+  };
+  const [newUser, setNewUser] = useState(defaultUser);
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState(null);
+  const [loading, setLoading] = useState("");
+
+  const handleChange = (e) => {
+    setNewUser({
+      ...newUser,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (newUser.password !== confirmPassword) {
+      setMessage("Passwords Do Not Match");
+    } else {
+      setMessage(null);
+      try {
+        const config = {
+          headers: {
+            "Content-type": "application/json",
+          },
+        };
+        setLoading(true);
+        const name = newUser.name;
+        const email = newUser.email;
+        const password = newUser.password;
+        const { data } = await axios.post(
+          "/api/users/",
+          { name, email, password },
+          config
+        );
+        localStorage.setItem("userInfo", JSON.stringify(data));
+        setLoading(false);
+      } catch (error) {
+        setError(error.response.data.message);
+        setLoading(false);
+      }
+    }
+    setNewUser(defaultUser);
+    setConfirmPassword("");
+  };
+
+  return (
+    <MainScreen title="Register">
+      <div className="login_container">
+        {message && <ErrorMessage variant="danger">{message}</ErrorMessage>}
+        {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+        {loading && <Loading />}
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              name="name"
+              value={newUser.name}
+              placeholder="Enter name"
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              value={newUser.email}
+              placeholder="Enter email"
+              name="email"
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={newUser.password}
+              placeholder="Enter password"
+              name="password"
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="confirmPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="confirmPassword"
+              value={confirmPassword}
+              placeholder="Confirm Password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Button className="mt-3" variant="primary" type="submit">
+            Register
+          </Button>
+        </Form>
+        <Row className="py-3">
+          <Col>
+            Already have an account? <Link to="/login">Login Here</Link>
+          </Col>
+        </Row>
+      </div>
+    </MainScreen>
+  );
 };
 
 export default RegisterPage;
